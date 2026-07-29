2973dc56ef1aeb655decf932760e8468e2ddc9b2
cil795624-dev <cil795624@gmail.com>
2026-07-29 18:09:01 +0800

    Add files via upload

--- editor-sw.js
+++ editor-sw.js
@@ -1,4 +1,4 @@
-const EDITOR_CACHE = "editor-order-manager-ios-20260729-kindfilters";
+const EDITOR_CACHE = "editor-order-manager-ios-20260729-earnings";
 const EDITOR_ASSETS = [
   "editor-order-manager.html",
   "editor-manifest.webmanifest",

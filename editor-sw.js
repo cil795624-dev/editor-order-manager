3a2403168d99d0ff7c5c36306bc43f8f78916db1
cil795624-dev <cil795624@gmail.com>
2026-07-29 21:04:08 +0800

    Add files via upload

--- editor-sw.js
+++ editor-sw.js
@@ -1,4 +1,4 @@
-const EDITOR_CACHE = "editor-order-manager-ios-20260729-todobuttons";
+const EDITOR_CACHE = "editor-order-manager-ios-20260729-rushlogic";
 const EDITOR_ASSETS = [
   "editor-order-manager.html",
   "editor-manifest.webmanifest",
